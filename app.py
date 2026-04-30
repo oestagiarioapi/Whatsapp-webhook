84fd95e3c6f66011076d2ce9ab3be0e89fcc54cc
oestagiarioapi <oestagiarioapi@gmail.com>
2026-04-30 17:22:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,19 @@ import os
 
 app = Flask(__name__)
 
+@app.route("/ping", methods=["GET"])
+def ping():
+    return "pong", 200
+    
+# 2. Rota do Webhook (para a Meta/Facebook validar)
 @app.route("/webhook", methods=["GET"])
-def verify():
+def webhook():
     token = request.args.get("hub.verify_token")
     if token == os.getenv("VERIFY_TOKEN"):
         challenge = request.args.get("hub.challenge")
-        return Response(challenge, mimetype='text/plain')  # ✅ linha 10
+        return Response(challenge, mimetype='text/plain')
     return "Forbidden", 403
+
+if _name_ == "_main_":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
